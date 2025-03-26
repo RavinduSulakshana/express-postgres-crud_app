42f2915418f54f9a6e3629cbf0e8e2826dac90ae
RavinduSulakshana <ravindusulakshana2525@gmail.com>
2025-03-26 05:46:24 +0530

    database connection intergration is completed

--- src/config/db.js
+++ src/config/db.js
@@ -1,12 +1,14 @@
 import pkg from "pg";
+import dotenv from "dotenv";
 const { Pool } = pkg;
+dotenv.config();
 
 const pool = new Pool({
-  user: process.env.DBUSER,
-  host: process.env.DBHOST,
-  database: process.env.DATABASE,
-  password: process.env.DBPASSWORD,
-  port: process.env.DBPORT,
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
 });
 
 pool.on("connect", () => {
--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import pool from "./config/db.js";
 
 dotenv.config();
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 
 // Middleware
 app.use(express.json());
@@ -13,6 +14,12 @@ app.use(cors());
 // Routes
 
 // Error handling
+
+//Testing postgress connection
+app.get("/", async (req, res) => {
+  const result = await pool.query("SELECT current_database()");
+  res.send(`The Database Name is : ${result.rows[0].current_database}`);
+});
 
 //Server Running
 app.listen(port, () => {
